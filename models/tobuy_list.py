65d122133c78337007642d6662393239750d0c40
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-01 17:45:06 +0100

    css added for index.html

--- models/tobuy_list.py
+++ models/tobuy_list.py
@@ -1,8 +1,8 @@
 from models.order import *
 
 
-order1 = Order("sandy", "may31", 2, "burger") 
-order2 = Order("moath", "12jun", 3, "pizza")
-order3 = Order("john", "24sep", 5, "kabab")
+order1 = Order("Sandy", "may31", 2, "burger")
+order2 = Order("Moath", "12jun", 3, "pizza")
+order3 = Order("John", "24sep", 5, "kabab")
 
-orders= [order1, order2, order3]
+orders = [order1, order2, order3]
