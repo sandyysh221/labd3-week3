48eac21fea70acae156a561382821096a2c2a988
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-01 17:22:56 +0100

    first route amended as was getting 404 error after forking

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,12 +4,12 @@ from app import app
 from models.tobuy_list import *
 
 
-@app.route("/orders")
+@app.route("/")
 def index():
-    return render_template("index.html", title="Home", orders=orders) 
+    return render_template("index.html", title="Home", orders=orders)
 
 
 @app.route("/orders/<index>")
 def order_index(index):
     order = orders[int(index)]
-    return render_template("order.html", title="Home", order=order) 
+    return render_template("order.html", title="Home", order=order)
